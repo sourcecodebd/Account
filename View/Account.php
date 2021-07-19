9815e3875e858986db56f9279b61544f00b17cf9
sourcecodebd <nafimahmud1996@gmail.com>
2021-07-20 01:29:17 +0600

    1st commit

---
+++ View/Account.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Account</title>
+<meta name="viewport", content="width=device-width, initial-scale=1">
+</head>
+<style>
+div#AccountBlock{width:270px;border:5px solid #ddd;padding:10px;margin:auto; text-align:center}
+body{background-image: url('../Images/G.gif'); background-repeat:no-repeat; background-size:cover}
+</style>
+<body>
+
+
+<div id="AccountBlock">
+
+<div style="background-image: url('../Images/F2.jpg');color:black; padding:20px;">
+
+<div style="background-color: #0A0F48;color:#00E9FF; padding:0px;">
+
+</div>
+
+<div style="background-color:#0A0F48;color:#00E9FF;padding:0px;font-family:calibri">
+<h1>Account</h1>
+</div>
+<b>
+<div style="background-image: url('../Images/F1.jpg');color:#00FFA8;padding:20px;font-size:18px">
+<br>
+<a href="Registration.php" style= "color:#00E9FF; font-family:calibri">Registration</a><br><br>
+<a href="Login.php" style= "color:#00E9FF; font-family:calibri">Login</a><br><br>
+</div>
+</b>
+
+<h6 style="font-size:10px; font-family: verdana; text-align: center; color:white"><?php include '../Footer/footer.php';?></h6>
+
+</div>
+</body>
+</html>
---
+++ View/LogIndex.php
@@ -0,0 +1,47 @@
+<?php
+session_start(); 
+if(empty($_SESSION["fname"])) 
+{
+header("Location: Login.php"); // Redirecting To Home Page
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Home</title>
+<meta name="viewport", content="width=device-width, initial-scale=1">
+</head>
+<style>
+div#LogIndexBlock{width:270px;border:5px solid #ddd;padding:10px;margin:auto;text-align:center;}
+body{background-image: url('../Images/G.gif'); background-repeat:no-repeat; background-size:cover; color:orange; font-size:18px}
+</style>
+
+<body>
+<div id="LogIndexBlock">
+<h1 style="color: #00E9FF; font-family: calibri">Home</h1>
+<style>
+h3{color:lightgreen}
+h5{color:#00E9FF}
+h6{color:white}
+</style>
+
+<h2 style="color:white">Welcome </h2> <h2 style="color:red"><?php echo  $_SESSION["fname"] . " " . $_SESSION["lname"]; ?><br></h2>
+<b>
+<?php
+  echo "<h3>Password:</h3>";
+  echo $_SESSION["password"];
+?>
+
+<h2 style="color: #B6FF00"><marquee direction=left>Do you want to go to </marquee></h2> <a href="LoginResult.php" style= "color:#00E9FF; font-family:calibri">Back</a>
+<h2 style="color: #B6FF00"><marquee direction=left>Do you want to</marquee></h2> <a href="../Control/logout.php" style= "color:#00E9FF; font-family:calibri">Log Out</a><br><br>
+
+<h6 style="font-size:10px; font-family: verdana; text-align: center"><?php include '../Footer/footer.php';?></h6>
+</b>
+
+</div>
+</body>
+</html>
+
+
